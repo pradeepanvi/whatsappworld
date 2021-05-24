981d62bc408dda2f97c00dab26dfa6fd997beea6
pradeepanvi <pradeepanvi@github.com>
2021-05-24 22:23:02 +0530

    handling different routes

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!')
-    next();
+app.use('/add-product', (req, res, next) => {
+    console.log('In the middleware!');
+    res.send('<h1>Products</h1>')
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('In the middleware!');
     res.send('<h1>Hello Pradeep</h1>')
 });
