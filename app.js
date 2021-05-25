df24a1806cf4eca7c98f335ca7dd64226baaf0bd
pradeepanvi <pradeepanvi@github.com>
2021-05-25 23:39:08 +0530

    add 404 route

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const notFoundRoutes = require('./routes/notFound');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+app.use(notFoundRoutes);
 
 app.listen(3000)
---
+++ routes/notFound.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.use('/', (req, res, next) => {
+    res.status(404).send('<h1>Not Found</h1>')
+})
+
+module.exports = router;
