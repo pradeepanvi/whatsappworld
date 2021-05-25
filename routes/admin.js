bff75178baea1de43c1289e3a000cec18df1e24b
pradeepanvi <pradeepanvi@github.com>
2021-05-25 23:58:17 +0530

    serving html pages using path

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,11 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
 // /admin/add-product => GET
 router.use('/add-product', (req, res, next) => {
-    res.send(
-        '<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add</button>'
-    )
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
