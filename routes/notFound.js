0fc8853065e1ebb037d2f16de2bd8b3a66679ae5
pradeepanvi <pradeepanvi@github.com>
2021-05-26 18:53:19 +0530

    404 html page

--- routes/notFound.js
+++ routes/notFound.js
@@ -1,8 +1,9 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
 router.use('/', (req, res, next) => {
-    res.status(404).send('<h1>Not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname, '../', 'views', '404.html'))
 })
 
 module.exports = router;
